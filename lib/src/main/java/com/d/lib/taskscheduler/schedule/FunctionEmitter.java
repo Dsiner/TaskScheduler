0f016878bbfda3ef689063d89be64ba90e309009
D <s90789@outlook.com>
2018-08-10 14:28:09 +0800

    Add postMain

--- lib/src/main/java/com/d/lib/taskscheduler/schedule/FunctionEmitter.java
+++ lib/src/main/java/com/d/lib/taskscheduler/schedule/FunctionEmitter.java
@@ -7,7 +7,6 @@ import com.d.lib.taskscheduler.callback.Function;
  * Created by D on 2018/5/16.
  */
 public class FunctionEmitter<T, R> extends Emitter {
-    public FunctionEmitter functionEmitter;
     public Function<? super T, ? extends R> function;
 
     public FunctionEmitter(Function<? super T, ? extends R> function, @Schedulers.Scheduler int scheduler) {
